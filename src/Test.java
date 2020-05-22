d3fe2d0c0aa8a82a1e1673f5a7e5761c0fa348e9
Mohammad <mohdali0990@gmail.com>
2020-05-22 05:38:13 -0500

    added fucntionality thats creates and writes in a file via arraylist

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,32 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("hello");
+        List<String> names = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Press 1 to add names\nPress 2 to exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            if(choice==1){
+                    System.out.println("Please add a name.");
+                    String addNames = scanner.nextLine();
+                    names.add(addNames);
+                    PrintWriter outputFile = new PrintWriter("saveName.txt");
+                    for (int i = 0; i < names.size(); i++) {
+                        outputFile.println(names.get(i));
+                    }
+                    outputFile.close();
+                }
+            else {
+                System.out.println("Thanks");
+                break;}
+            }
     }
 }
